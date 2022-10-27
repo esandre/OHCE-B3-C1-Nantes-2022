1023655302f4d051711c0e85301c10769123be9d
Enzo Sandré <atelier@enzosandre.fr>
2022-10-27 14:51:42 +0200

    Tests de renvoi en miroir

--- OHCE.Test/OhceTest.cs
+++ OHCE.Test/OhceTest.cs
@@ -2,9 +2,26 @@
 {
     public class OhceTest
     {
-        [Fact]
-        public void TestVide()
+        [Fact(DisplayName = "QUAND on saisit une chaîne 'toto'" +
+                            "ALORS celle-ci est renvoyée en miroir")]
+        public void TestRenvoiMiroirToto()
         {
+            // QUAND on saisit une chaîne
+            var resultat = Ohce.Palindrome("toto");
+
+            // ALORS celle-ci est renvoyée en miroir
+            Assert.Equal("otot", resultat);
+        }
+
+        [Fact(DisplayName = "QUAND on saisit une chaîne 'test'" +
+                            "ALORS celle-ci est renvoyée en miroir")]
+        public void TestRenvoiMiroirTest()
+        {
+            // QUAND on saisit une chaîne
+            var resultat = Ohce.Palindrome("test");
+
+            // ALORS celle-ci est renvoyée en miroir
+            Assert.Equal("tset", resultat);
         }
     }
 }
---
+++ OHCE/Ohce.cs
@@ -0,0 +1,7 @@
+﻿namespace OHCE;
+
+public static class Ohce
+{
+    public static string Palindrome(string mot) 
+        => new (mot.Reverse().ToArray());
+}
